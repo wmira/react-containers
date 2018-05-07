044ae70ca13414a2bb3d00f5e908aa6acfe32ba9
Warren <warrenmira@gmail.com>
2018-05-07 23:04:14 +0800

    more cleanups

--- src/LeftRightSection.tsx
+++ src/LeftRightSection.tsx
@@ -1,32 +1,30 @@
+import * as React from 'react'
+import styled from 'styled-components'
 
+const { Children } = React
 
-// import React, { Children } from 'react';
-// import PropTypes from 'prop-types'
-// import styled from 'styled-components';
+const LeftRightContainer = styled.div`
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: space-between;
+`
+const Empty = () => (<div></div>)
 
-// const LeftRightContainer = styled.div`
-//     width: 100%;
-//     height: 100%;
-//     display: flex;
-//     justify-content: space-between;
-// `
-// const Empty = () => <div></div>;
+export interface LeftRightSectionProps {
+    children?: React.ReactNode
+    style?: React.CSSProperties,
+    className?: string
+}
 
+export const LeftRightSection: React.SFC<LeftRightSectionProps> = (props) => {
+    const children = Children.toArray(props.children || []);    
+    return (
+        <LeftRightContainer>
+            { children[0] ? children[0] : <Empty /> }
+            { children[1] ? children[1] : <Empty /> }
+        </LeftRightContainer>
+    );
+}
 
-// export const LeftRightSection = (props) => {
-//     const children = Children.toArray(props.children);    
-//     return (
-//         <LeftRightContainer>
-//             { children[0] ? children[0] : <Empty /> }
-//             { children[1] ? children[1] : <Empty /> }
-//         </LeftRightContainer>
-//     );
-// }
-
-// LeftRightSection.propTypes = {
-//     children: PropTypes.node,
-//     style: PropTypes.object,
-//     className: PropTypes.string
-// };
-
-// export default LeftRightSection;
+export default LeftRightSection
--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,6 @@
 
 export { Center } from './Center'
 export { RenderIf } from './RenderIf'
-//export { LeftRightSection } from './LeftRightSection';
-//export { InlineItems } from './InlineItems';
+export { LeftRightSection } from './LeftRightSection';
+export { InlineItems } from './InlineItems';
 export { centerChildren } from './centerChildren'
