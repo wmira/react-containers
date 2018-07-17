f774fc03a909efff3f669b1e1c3c7cdc816e1b36
Warren <warrenmira@gmail.com>
2018-07-17 22:23:09 +0800

    updated inline items

--- src/InlineItems.tsx
+++ src/InlineItems.tsx
@@ -1,21 +1,32 @@
 
 import * as React from 'react';
+import styled from 'styled-components';
 
 const { Children } = React
 
 export interface InlineItemsProps {
     rAlign?: boolean //is it right align?, default is false
-    padding?: number, //the default padding
-    children?: React.ReactNode
+    padding?: number //the default padding
+    children?: React.ReactNode    
+    container?: React.ReactType
 }
 
+const InlineContainer = styled.div`
+    display: flex;
+    flex-direction: row;
+`
+
 export const InlineItems: React.SFC<InlineItemsProps> = (props: InlineItemsProps ) => (
-    <div style={{display: 'inline-block' }}>
+    <InlineContainer>
         { Children.toArray(props.children || []).map( (child, idx) => {
             const spacerField = props.rAlign ? 'paddingLeft' : 'paddingRight';
-            return <div key={idx} style={{ display: 'inline-block', [spacerField]: props.padding }}>{ child }</div>;
+            const ContainerToUse = props.container
+            if ( ContainerToUse ) {
+                return <ContainerToUse style={{ [spacerField]: props.padding }} key={idx} >{ child }</ContainerToUse>
+            }
+            return <div style={{ [spacerField]: props.padding }}>{ child }</div>;
         })}
-    </div>
+    </InlineContainer>
 );
 
 InlineItems.defaultProps = {
--- src/playground/index.tsx
+++ src/playground/index.tsx
@@ -2,6 +2,7 @@
 import * as React from 'react'
 import { render } from 'react-dom'
 import { Center } from '../'
+import { InlineItems } from '../InlineItems';
 
 export const CenterDemo = () => (
     <div style={{width: 300, height: 300, border: '1px solid #AAA'}}>
@@ -11,12 +12,28 @@ export const CenterDemo = () => (
     </div>
 )
 
+export const InlineDemo = () => (
+    <div style={{ width: 40, height: 300, border: '1px solid #AAA'}}>
+        <InlineItems container={Center}>
+            <div>1</div>
+            <div>2</div>
+            <div>3</div>
+        </InlineItems>
+    </div>
+)
+
 const renderPlayground = () => {
+    
     const element = document.getElementById('app')
-    render( (
-        <div style={{width: '100%', height: '100%'}}>
-            <CenterDemo/>
-        </div>
-    ), 
+    render( 
+        <>
+            <div style={{width: 'width: 100%', height: '100%'}}>            
+                <CenterDemo/>
+                <InlineDemo/>
+            </div>            
+        </>
+    , 
     element)
 }
+
+renderPlayground()
